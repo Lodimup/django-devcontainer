24daca968b782e532f7833a572a45ccc4092f16c
Pumidol <pumidol.l@gmail.com>
2023-03-03 07:48:48 +0000

    CHANGE better post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo 'export $(cat /workspaces/django-devcontainer/.env.local | xargs)' >> ~/.bashrc
+PRE='export $(cat '
+POST='/.env.local | xargs)'
+CMD=$PRE$1$POST
+echo $CMD >> ~/.bashrc
+
 git config --global --add safe.directory /workspace
 git config core.fileMode false
 poetry config virtualenvs.in-project true
