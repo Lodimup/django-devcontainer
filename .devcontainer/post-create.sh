a4f3c1e7debfd9d1737277177748acaafa1ee776
Pumidol <pumidol.l@gmail.com>
2023-04-26 17:00:04 +0000

    MV git config out of post-create

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,7 +5,5 @@ POST='/.env.local | xargs)'
 CMD=$PRE$1$POST
 echo $CMD >> ~/.bashrc
 
-git config --global --add safe.directory /workspace
-git config core.fileMode false
 poetry config virtualenvs.in-project true
 poetry install
